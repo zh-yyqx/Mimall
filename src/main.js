247ec3c6f3e592ecf8ee6bb5e3d250e83cbd238b
“ <573073012@qq.com>
2022-02-18 01:06:13 +0800

    接口学习

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
 
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000;
+
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+    let res = response.data;
+    if(res.status==0){
+      return res.data
+    }else if(res.status==10){//未登录的错误
+      //因为在main.js用路由跳转是没用的，路由是挂载到vue实例上的， 不能使用this.$router.push，this并不指向vue
+      //且vue路由是哈希路由#,所以用window.location.href
+      window.location.href='/#/login';
+    }else{
+      alert(res.msg)
+    }
+})
+
+Vue.use(VueAxios,axios)
+// 生产环境的提示
 Vue.config.productionTip = false
 
 new Vue({
